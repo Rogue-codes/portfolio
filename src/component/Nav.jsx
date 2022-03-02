4dac0a2e92828ab474c9e661f70003738543c778
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-03-02 11:25:07 +0100

    mobile

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useTransition, animated } from 'react-spring'
+import {useState} from 'react'
 
 function Nav() {
+  const [showMenu, setShowMenu]= useState(false)
+
+  const maskTransitions = useTransition(showMenu, {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 },
+      reverse: showMenu,
+      delay: 200,
+      // config: config.molasses,
+      // onRest: () => set(!show),
+    })
+
+    const menuTransitions = useTransition(showMenu, {
+      from: { opacity: 0, transform: "translatey(-100%)"},
+      enter: { opacity: 1, transform: "translatey(60%)" },
+      leave: { opacity: 0, transform: "translatey(-100%)" },
+      reverse: showMenu,
+      delay: 200,
+      // config: config.molasses,
+      // onRest: () => set(!show),
+    })
+
+
   return (
     <nav>
         <a href="#home">HOME</a>
@@ -8,6 +35,26 @@ function Nav() {
         <a href="#portfolio">PORTFOLIO</a>
         <a href="#contact">CONTACT</a>
         <a href="https://github.com/Rogue-codes/">GIT HUB</a>
+        <span className="icon">
+              <FontAwesomeIcon icon={faBars}
+              onClick= {() => setShowMenu(!showMenu)}/>
+        </span>
+
+        {
+          maskTransitions((styles, item) => item && <animated.div style={styles} className='firstAimate' onClick = {()=> setShowMenu(false)}>
+
+          </animated.div>)
+        }
+
+        {
+          menuTransitions((styles, item) => item && <animated.div style={styles} className='Animate'>
+            <a href="#home" onClick = {()=> setShowMenu(false)}>HOME</a>
+            <a href="#abt" onClick = {()=> setShowMenu(false)}>ABOUT</a>
+            <a href="#portfolio" onClick = {()=> setShowMenu(false)}>PORTFOLIO</a>
+            <a href="#contact" onClick = {()=> setShowMenu(false)}>CONTACT</a>
+            <a href="https://github.com/Rogue-codes/" onClick = {()=> setShowMenu(false)}>GIT HUB</a>
+          </animated.div>)
+        }
     </nav>
   )
 }
