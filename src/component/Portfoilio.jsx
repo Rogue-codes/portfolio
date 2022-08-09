afe4535d90bb519c316ee1fb22c36ddb76e1a199
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-08-09 04:28:50 +0100

    fix

--- src/component/Portfoilio.jsx
+++ src/component/Portfoilio.jsx
@@ -16,7 +16,7 @@ function Portfoilio() {
             </h1>
 
             <div className="container">
-                <a href="https://sa-market.netlify.app/" target="_blank" className="left" data-aos="fade-right">
+                <a href="https://sa-market.netlify.app/" target="_blank" rel="noreferrer" className="left" data-aos="fade-right">
                     <div>
                         <div className="shadow"></div>
                         <img src="/samarket.png" alt="" />
@@ -61,7 +61,7 @@ function Portfoilio() {
                     </div>
                 </div>
 
-                <a href="https://healthpro.netlify.app/" target="_blank" className="left" data-aos="fade-left">
+                <a href="https://healthpro.netlify.app/" target="_blank" rel="noreferrer" className="left" data-aos="fade-left">
                     <div>
                         <div className="shadow"></div>
                         <img src="/health.png" alt="" />
@@ -111,88 +111,88 @@ function Portfoilio() {
                     <h1>Back to School</h1>
                     <p>A simple Application built with Vanila js , HTML and CSS.. This application is aimed at helping young university under grads to calculate their GPA. </p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://backtocollege.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://backtocollege.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Cloudther</h1>
                     <p>A weather Application that shows the weather condition of a place. The App Consumes the open weather API and shows weather data based on the user's location.</p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://cloudther.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://cloudther.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Orange Bank</h1>
                     <p>Cloned the Website of Titan-Trust Bank and produced a pixel perfect replica of the original website. Project was created using React and SCSS</p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://orangebank.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://orangebank.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>BlueBirdCmast</h1>
                     <p>Built the official website of BlueBirdCmast an I.T company that provide cloudservices to various organizations and enterprises</p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://bluebirdcmast.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://bluebirdcmast.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Minnato Exchange</h1>
                     <p>A currency converter Application Built with HTML, CSS and Vanilla Js. The Application converts value between differnt currencies with the help of data coming from free currency API </p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://minnatoexchange.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://minnatoexchange.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Lexus WEbsite clone</h1>
                     <p>Here I cloned the official homepage of Lexus website using <strong>HTML, CSS and JavaScript</strong> </p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://lexus-clone.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://lexus-clone.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Netflix Home page clone</h1>
                     <p>Here I cloned the official homepage of Netflix Nigeria website using <strong>React and SCSS</strong> </p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://nettflixx-clone.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://nettflixx-clone.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Arfri Muzik</h1>
                     <p>A music Application built with HTML, CSS and JavaScript. The App supports Play, Pause, Next, Previous and Repeat functionalities</p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://arfimuzik.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://arfimuzik.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Payzone</h1>
                     <p>A Fintech based website built with Reactjs and styled-components added some built in NPM libraries to create a slider component</p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://payzone.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://payzone.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Techy Point</h1>
                     <p>Cloned the Homepage of AltSchool Africa using <b>React and SCSS</b>. This project helped me master my CSS positioning and animation skills.</p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://techypoint.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://techypoint.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
 
                 <div className="cards" data-aos="fade-up">
                     <h1>Rogue Movie</h1>
                     <p>A Movie Application that consumes data from the OMDB API and displays current movies. The App was built using <b>Reactjs and Styled-components</b> </p>
                     <FolderOutlined className="file"/>
-                    <a href="https://github.com/Rogue-codes" target="_blank" className="git"><GitHub/> </a>
-                    <a href="https://rogue-movies.netlify.app/" target="_blank" className="url"><Link/></a>
+                    <a href="https://github.com/Rogue-codes" target="_blank" rel="noreferrer" className="git"><GitHub/> </a>
+                    <a href="https://rogue-movies.netlify.app/" target="_blank" rel="noreferrer" className="url"><Link/></a>
                 </div>
             </div>
             <button className='show' onClick={() =>setIsClicked(!isClicked)}>{isClicked ? 'Show Less' : 'Show More'}</button>
